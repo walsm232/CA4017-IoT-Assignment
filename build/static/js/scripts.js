959cf7b8d7d50041b41084799ebe53f9f3cb138e
Michael <michael.walsh232@mail.dcu.ie>
2021-04-08 21:46:43 +0100

    Message sending functionality now working and showing received messages in a Bootstrap card text field

--- build/static/js/scripts.js
+++ build/static/js/scripts.js
@@ -1,1 +1,19 @@
 /* JavaScript */
+
+function runScript() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                alert('Successfully ran the MQTT application' + request.responseText);
+            } else {
+                alert('Application failed to launch' + request.status);
+            }
+        }
+    };
+    request.open('POST', '../../app.py', true);
+    request.send(null);
+    return false;  
+};
+
+document.getElementById('script-button').onclick = runScript;
