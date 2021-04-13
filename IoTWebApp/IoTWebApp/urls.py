e4b625e9301e4317012c53dcb4f8fae842f5fc12
Michael <michael.walsh232@mail.dcu.ie>
2021-04-13 18:49:24 +0100

    Pushing finished IoT Device Manager webapp

--- IoTWebApp/IoTWebApp/urls.py
+++ IoTWebApp/IoTWebApp/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import Homepage
+from .views import Homepage, Connect_Device
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Homepage, name='Homepage'),
+    path('connect/', Connect_Device, name='ConnectDevice'),
 ]
--- IoTWebApp/IoTWebApp/views.py
+++ IoTWebApp/IoTWebApp/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+import subprocess
+
+from django.core import management
 
 def Homepage(request):
     return render(request, 'main/index.html')
+
+def Connect_Device(request):
+    if request.POST:
+        handle = subprocess.Popen([
+        'powershell.exe',
+        'C:/Users/Michael/Desktop/CA4017-IoT-Assignment/IoTWebApp/IoTWebApp/start.ps1',
+        ], stdout=subprocess.PIPE)
+        output = handle.stdout.read().decode('utf-8')
+    return HttpResponse(output)
